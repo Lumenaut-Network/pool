87d2269cd5458cbf13eba12308dc59a706dabf4a
Tex <texmoran2@gmail.com>
2018-01-15 22:20:37 +0800

    Finish account creation tool

--- createpool.py
+++ createpool.py
@@ -2,69 +2,132 @@ from stellar_base.keypair import Keypair
 from stellar_base.transaction import Transaction
 from stellar_base.transaction_envelope import TransactionEnvelope as Te
 from stellar_base.operation import SetOptions
+from stellar_base.operation import CreateAccount
 from stellar_base.horizon import horizon_testnet, horizon_livenet
 import requests, sys
 
-signers = []
-with open('signers.txt') as f:
-	signers = f.read().splitlines()
+SIGNING_THRESHOLD = {
+	"low": 3,
+	"medium": 3,
+	"high": 4
+}
+STARTING_BALANCE = "5"
 
-kp = None
-if len(sys.argv) > 1:
-	desired_tail = sys.argv[1]
-
-	print("looking for address ending in '" + desired_tail + "'...")
-	while True:
-		kp = Keypair.random()
-		if kp.address().decode()[-len(desired_tail):] == desired_tail:
-			break
-else:
-	kp = Keypair.random()
-
-pool_address = kp.address().decode()
-
-r = requests.get('https://horizon-testnet.stellar.org/friendbot?addr=' + pool_address)
-print("Requesting initial funding...")
-
-threshold = 3
-operations = [
-	SetOptions({
-		"signer_address": address,
-		"signer_weight": 1,
-		"signer_type": "ed25519PublicKey",
-		"source_account": kp.address().decode()
-		}) 
-	for address in signers
-]
-
-operations.append(
-	SetOptions({
-		"source_account": kp.address().decode(),
-		"home_domain": bytes("lumenaut.net", "utf-8"),
-		"master_weight": 1, 
-		"low_threshold": threshold, 
-		"med_threshold": threshold, 
-		"high_threshold": threshold,
-		"inflation_dest": kp.address().decode()
-	})
-)
-
+network = "TESTNET"
 horizon = horizon_testnet()
-sequence = horizon.account(pool_address).get('sequence')
-tx = Transaction(
-	source = pool_address,
-	opts = {
-		'sequence': sequence,
-		'fee': 100 * len(operations),
-		'operations': operations,
-	},
-)
-envelope = Te(tx = tx, opts = {"network_id": "TESTNET"})
-envelope.sign(kp)
-xdr = envelope.xdr()
 
-print("Submitting xdr:")
-print(xdr.decode("utf-8"))
+def generate_pool_keypair():
+	kp = None
 
-response = horizon.submit(xdr)
-print("Transaction href: ", response["_links"]["transaction"]["href"])
+	if len(sys.argv) > 1:
+		desired_tail = sys.argv[1]
+
+		print("Looking for address ending in '" + desired_tail + "'...")
+		while True:
+			kp = Keypair.random()
+			if kp.address().decode()[-len(desired_tail):] == desired_tail:
+				break
+	else:
+		kp = Keypair.random()
+
+	return kp
+
+def create_pool_account(pool_keypair):
+	funding_account_kp = None
+	try:
+		funding_account_kp = Keypair.from_seed(input("Funding acount secret key: "))
+	except:
+		print("Invalid secret key, aborting")
+		return False
+
+
+	creation_operation = CreateAccount({
+		"destination": pool_keypair.address().decode(),
+		"starting_balance": STARTING_BALANCE
+	})
+
+	sequence = horizon.account(funding_account_kp.address().decode()).get('sequence')
+
+	tx = Transaction(
+		source = funding_account_kp.address().decode(),
+		opts = {
+			'sequence': sequence,
+			'operations': [creation_operation],
+		},
+	)
+
+	envelope = Te(tx = tx, opts = {"network_id": network})
+	envelope.sign(funding_account_kp)
+
+	xdr = envelope.xdr()
+	response = horizon.submit(xdr)
+	if "_links" in response:
+		print("Creation of account transaction link: " + response["_links"]["transaction"]["href"])
+		return True
+	else:
+		print("Failed to create account. Dumping response:")
+		print(response)
+		return False
+
+def get_signers():
+	signers = []
+	with open('signers.txt') as f:
+		signers = f.read().splitlines()
+	return signers
+
+def set_account_signers(pool_keypair, threshold):
+	pool_address = pool_keypair.address().decode()
+
+	operations = [
+		SetOptions({
+			"signer_address": signer_address,
+			"signer_weight": 1,
+			"signer_type": "ed25519PublicKey",
+			"source_account": pool_address
+			}) 
+		for signer_address in get_signers()
+	]
+
+	operations.append(
+		SetOptions({
+			"source_account": pool_address,
+			"home_domain": bytes("lumenaut.net", "utf-8"),
+			"master_weight": 0, 
+			"low_threshold": threshold["low"], 
+			"med_threshold": threshold["medium"], 
+			"high_threshold": threshold["high"],
+			"inflation_dest": pool_address
+		})
+	)
+
+	sequence = horizon.account(pool_address).get('sequence')
+	tx = Transaction(
+		source = pool_address,
+		opts = {
+			'sequence': sequence,
+			'fee': 100 * len(operations),
+			'operations': operations,
+		},
+	)
+
+	envelope = Te(tx = tx, opts = {"network_id": network})
+	envelope.sign(pool_keypair)
+
+	xdr = envelope.xdr()
+	response = horizon.submit(xdr)
+
+	if "_links" in response:
+		print("Set options transaction href: ", response["_links"]["transaction"]["href"])
+		print("Created successfully!")
+	else:
+		print("Failed to set account signers, dumping response:")
+		print(response)
+
+def main():
+	pool_kp = generate_pool_keypair()
+	print("Pool keypair: " + pool_kp.address().decode() + " | " + pool_kp.seed().decode())
+
+	if create_pool_account(pool_kp):
+		set_account_signers(pool_kp, SIGNING_THRESHOLD)
+
+if __name__ == "__main__": main()
