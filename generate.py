e482c04aa77b9c20b31887008fdf1868bafb13e4
Tex <texmoran2@gmail.com>
2018-01-15 16:53:10 +0800

    finalise payment calculations & properly use inflation received. pretty print stats out

--- generate.py
+++ generate.py
@@ -1,6 +1,5 @@
 import sqlite3, json, base64
 from decimal import *
-from math import min
 
 from stellar_base.asset import Asset
 from stellar_base.keypair import Keypair
@@ -19,39 +18,49 @@ select_account_op = "SELECT `accounts`.`accountid`, `balance`, `datavalue` FROM 
 horizon = horizon_testnet()
 donations = {}
 
+BASE_FEE = 100
+XLM_STROOP = 10000000
+
 def XLM_Decimal(n):
-	return Decimal(n).quantize(Decimal('.0000001'), rounding=ROUND_DOWN)
+	return Decimal(n).quantize(Decimal('.0000001'), rounding=ROUND_DOWN) # 7 decimal places is the longest supported
 
 def add_donation(donation, payout):
 	donation = base64.b64decode(donation).decode("utf-8")
 	pct, address = Decimal(min(int(donation[:3]), 100) / 100).quantize(Decimal('.01')), donation[3:]
 	amt = XLM_Decimal(payout * pct)
+
 	if address in donations.keys():
 		donations[address] += amt
 	else:
 		donations[address] = amt
+
 	return pct
 
-def calculate_payout(cur, aid, bal, donation):
-	bal /= 10000000 # amount in lumens
-	payout = XLM_Decimal(bal * 0.01 / 52)
+def calculate_payout(cur, inflation, total_balance, aid, bal, donation):
+	bal = Decimal(bal - BASE_FEE * 2) / XLM_STROOP # amount in lumens (take 200 stroops each to cover transaction fees)
+	bal_pct = bal / total_balance # higher precision
+	payout = XLM_Decimal(inflation * bal_pct)
 
 	donation_cut = 0
 	if donation:
 		donation_cut = add_donation(donation, payout)
+
 	donation_cut = XLM_Decimal(payout * donation_cut)
 
 	return payout - donation_cut
 
-def accounts_payout(conn, pool_addr, size=100):
+def accounts_payout(conn, pool_addr, inflation, size=100):
 	cur = conn.cursor()
+	cur.execute("SELECT Sum(balance) FROM accounts WHERE `inflationdest`=?", (pool_address, ))
+	total_balance = Decimal(cur.fetchone()[0]) / XLM_STROOP
+
 	cur.execute(select_account_op, (pool_addr, ))
 	payouts = []
 	while True:
 		batch = cur.fetchmany(size)
 		if not batch or batch == ():
 			break
-		payouts.append([(aid, calculate_payout(cur, aid, balance, donation)) for aid, balance, donation in batch])
+		payouts.append([(aid, calculate_payout(cur, inflation, total_balance, aid, balance, donation)) for aid, balance, donation in batch])
 
 	donation_payouts = []
 	batch = []
@@ -69,15 +78,18 @@ def accounts_payout(conn, pool_addr, size=100):
 def make_payment_op(account_id, amount):
 	return Payment({'destination': account_id, 'amount': str(amount), 'asset': Asset('XLM')})
 
-def main():
+def main(inflation):
+	inflation = XLM_Decimal(inflation)
+
 	conn = sqlite3.connect(db_address)
 	transactions = []
 
 	sequence = horizon.account(pool_address).get('sequence')
 	total_payments_cost = 0
+	num_payments = 0
 	total_fee_cost = 0
 
-	for batch in accounts_payout(conn, pool_address):
+	for batch in accounts_payout(conn, pool_address, inflation):
 		tx = Transaction(
 			source = pool_address,
 			opts = {
@@ -86,14 +98,29 @@ def main():
 			}
 		)
 		tx.fee = len(tx.operations) * 100
-		total_fee_cost += tx.fee
-		total_payments_cost += sum([Decimal(payment.amount) for payment in tx.operations])
 		envelope = Te(tx = tx, opts = {"network_id": network})
 		transactions.append(envelope.xdr().decode("utf-8"))
+
+		total_fee_cost += tx.fee / XLM_STROOP
+		total_payments_cost += sum([Decimal(payment.amount) for payment in tx.operations])
+		num_payments += len(tx.operations)
+
 		sequence = int(sequence) + 1
+
+	print(
+		"Stats: \n\
+		Inflation received: " + str(inflation) + "\n\
+		A total of " + str(total_payments_cost) +\
+		" XLM paid over " + str(num_payments) +\
+		" inflation payments using " + str(total_fee_cost) + " XLM in fees. \n\
+		People donated " + str(sum([n for n in donations.values()])) +\
+		" XLM to " + str(len(donations.keys())) +\
+		" different addresses.\n"
+	)
 
 	with open("transactions.dat", 'w') as outf:
 		json.dump(transactions, outf)
 	print("Done. Output to transactions.dat")
 
-if __name__ == '__main__': main()
+TEST_AMT = 49855.2650163
+if __name__ == '__main__': main(TEST_AMT)
--- watch.py
+++ watch.py
@@ -1,4 +1,4 @@
-import json
+import json, requests
 from generate import main as start_payout
 from stellar_base.horizon import horizon_testnet, horizon_livenet
 
@@ -7,14 +7,31 @@ print("Connecting to horizon-testnet...")
 horizon = horizon_testnet()
 stream = horizon.operations(sse = True)
 INFLATION_TYPE = 9
+POOL_ADDRESS = "GA3FUYFOPWZ25YXTCA73RK2UGONHCO27OHQRSGV3VCE67UEPEFEDCOPA"
 
 print("Connected! Watching for inflation operation...")
 
+def investigate_inflation(effects_link):
+	res = requests.get(effects_link)
+	effects = res.json()["_embedded"]["records"]
+	found = False
+	for record in effects:
+		if record["account"] == POOL_ADDRESS:
+			print("Paying out " + record["amount"])
+			start_payout(record["amount"])
+			found = True
+	if not found:
+		print("Didn't find account " + POOL_ADDRESS)
+		print("Look for yourself: " + effects_linkf)
+		
+investigate_inflation("https://horizon.stellar.org/operations/66149948126683137/effects")
+'''
 for response in stream:
 	data = json.loads(response.data)
 	if type(data) is not dict: continue
 
 	type_i = data["type_i"]
 	if type_i == INFLATION_TYPE:
-		print("Inflation triggered, starting payout...")
-		start_payout()
+		print("Inflation triggered, investigating...")
+		investigate_inflation(data["_links"]["effects"]["href"])
+'''	
