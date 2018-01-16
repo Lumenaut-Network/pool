5d9feec5126962ad58c22e86e0ce276e256fe243
Simon de Haan <simon@praekelt.org>
2018-01-16 10:31:07 +0100

    fix flake8 violations for sign.py

--- sign.py
+++ sign.py
@@ -1,26 +1,27 @@
-import json, base64
+import json
 from stellar_base.builder import Builder
-from stellar_base.keypair import Keypair
-from stellar_base.horizon import horizon_testnet, horizon_livenet
+from stellar_base.horizon import horizon_testnet
 
 network = "TESTNET"
+horizon = horizon_testnet()
 
 transactions_unsigned = []
 with open("transactions.json", 'r') as tfile:
 	transactions_unsigned = json.load(tfile)
 
 key = input("Signing Key: ")
+builder = Builder(secret=key, network=network)
 amt_signed = 0
 needed = 3
 transactions = []
 for xdr in transactions_unsigned:
-	builder = Builder(secret = key, network = network)
 	builder.import_from_xdr(xdr)
 	amt_signed = len(builder.te.signatures) + 1
 	builder.sign()
 	transactions.append(builder.gen_xdr().decode("utf-8"))
 
-print("Added signature. Transactions now signed by %s/%s parties." % (str(amt_signed), str(needed)))
+print("Added signature. Transactions now signed by %s/%s parties." % (
+	str(amt_signed), str(needed)))
 
 
 if amt_signed >= needed:
@@ -28,8 +29,9 @@ if amt_signed >= needed:
 	i = 1
 	for xdr in transactions:
 		try:
-			response = horizon.submit(self.gen_xdr())
-			print("Transaction " + str(i) + " link: ", response["_links"]["transaction"]["href"])
+			response = horizon.submit(builder.gen_xdr())
+			print("Transaction %s link: %s" % (
+				str(i), response["_links"]["transaction"]["href"]))
 			i += 1
 		except Exception as e:
 			raise e
