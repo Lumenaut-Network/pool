bd3d57b975a804e615c1e310699d67ab6235ee84
Simon de Haan <simon@praekelt.org>
2018-01-16 10:00:45 +0100

    fix flake8 violations for generate.py

--- generate.py
+++ generate.py
@@ -1,32 +1,40 @@
-import sqlite3, json, base64
-from decimal import *
+import sqlite3
+import json
+import base64
+
+from decimal import Decimal, ROUND_DOWN
 
 from stellar_base.asset import Asset
-from stellar_base.keypair import Keypair
 from stellar_base.operation import Payment
 from stellar_base.transaction import Transaction
 from stellar_base.transaction_envelope import TransactionEnvelope as Te
-from stellar_base.memo import TextMemo
-from stellar_base.horizon import horizon_testnet, horizon_livenet
+from stellar_base.horizon import horizon_testnet
 
 pool_address = "GCFXD4OBX4TZ5GGBWIXLIJHTU2Z6OWVPYYU44QSKCCU7P2RGFOOHTEST"
 network = "TESTNET"
 db_address = "../core/stellar.db"
-select_account_op = "SELECT `accounts`.`accountid`, `balance`, `datavalue` FROM `accounts` \
-					 LEFT JOIN `accountdata` ON `accountdata`.`accountid` = `accounts`.`accountid` AND `dataname`='lumenaut.net donation' \
-					 WHERE `inflationdest`=?"
+select_account_op = """
+	SELECT `accounts`.`accountid`, `balance`, `datavalue` FROM `accounts`
+	LEFT JOIN `accountdata`
+		ON `accountdata`.`accountid` = `accounts`.`accountid`
+		AND `dataname`='lumenaut.net donation'
+	WHERE `inflationdest`=?"""
 horizon = horizon_testnet()
 donations = {}
 
 BASE_FEE = 100
 XLM_STROOP = 10000000
 
+
 def XLM_Decimal(n):
-	return Decimal(n).quantize(Decimal('.0000001'), rounding=ROUND_DOWN) # 7 decimal places is the longest supported
+	# 7 decimal places is the longest supported
+	return Decimal(n).quantize(Decimal('.0000001'), rounding=ROUND_DOWN)
+
 
 def add_donation(donation, payout):
 	donation = base64.b64decode(donation).decode("utf-8")
-	pct, address = Decimal(min(int(donation[:3]), 100) / 100).quantize(Decimal('.01')), donation[3:]
+	pct, address = Decimal(min(int(donation[:3]), 100) / 100).quantize(
+		Decimal('.01')), donation[3:]
 	amt = XLM_Decimal(payout * pct)
 
 	if address in donations.keys():
@@ -35,6 +43,7 @@ def add_donation(donation, payout):
 		donations[address] = amt
 
 	return pct
+
 
 def calculate_payout(cur, inflation, total_balance, aid, bal, donation):
 	fee = BASE_FEE * (donation and 2 or 1)
@@ -50,9 +59,11 @@ def calculate_payout(cur, inflation, total_balance, aid, bal, donation):
 
 	return payout - donation_cut
 
+
 def accounts_payout(conn, pool_addr, inflation, size=100):
 	cur = conn.cursor()
-	cur.execute("SELECT Sum(balance) FROM accounts WHERE `inflationdest`=?", (pool_address, ))
+	cur.execute("SELECT Sum(balance) FROM accounts WHERE `inflationdest`=?", (
+		pool_address, ))
 	total_balance = Decimal(cur.fetchone()[0]) / XLM_STROOP
 
 	cur.execute(select_account_op, (pool_addr, ))
@@ -61,7 +72,10 @@ def accounts_payout(conn, pool_addr, inflation, size=100):
 		batch = cur.fetchmany(size)
 		if not batch or batch == ():
 			break
-		payouts.append([(aid, calculate_payout(cur, inflation, total_balance, aid, balance, donation)) for aid, balance, donation in batch])
+		payouts.append([
+			(aid,
+			 calculate_payout(cur, inflation, total_balance, aid, balance, donation))
+			for aid, balance, donation in batch])
 
 	donation_payouts = []
 	batch = []
@@ -77,7 +91,11 @@ def accounts_payout(conn, pool_addr, inflation, size=100):
 
 
 def make_payment_op(account_id, amount):
-	return Payment({'destination': account_id, 'amount': str(amount), 'asset': Asset('XLM')})
+	return Payment({
+		'destination': account_id,
+		'amount': str(amount),
+		'asset': Asset('XLM')})
+
 
 def main(inflation):
 	inflation = XLM_Decimal(inflation)
@@ -92,30 +110,31 @@ def main(inflation):
 
 	for batch in accounts_payout(conn, pool_address, inflation):
 		tx = Transaction(
-			source = pool_address,
-			opts = {
+			source=pool_address,
+			opts={
 				'sequence': sequence,
 				'operations': [make_payment_op(aid, amount) for aid, amount in batch]
 			}
 		)
 		tx.fee = len(tx.operations) * 100
-		envelope = Te(tx = tx, opts = {"network_id": network})
+		envelope = Te(tx=tx, opts={"network_id": network})
 		transactions.append(envelope.xdr().decode("utf-8"))
 
 		total_fee_cost += XLM_Decimal(tx.fee) / XLM_STROOP
-		total_payments_cost += sum([XLM_Decimal(payment.amount) for payment in tx.operations])
+		total_payments_cost += sum([
+			XLM_Decimal(payment.amount) for payment in tx.operations])
 		num_payments += len(tx.operations)
 
 		sequence = int(sequence) + 1
 
 	print(
-		"Stats: \n\
-		Inflation received: " + str(inflation) + "\n\
-		A total of " + str(total_payments_cost) +\
-		" XLM paid over " + str(num_payments) +\
-		" inflation payments using " + str(total_fee_cost) + " XLM in fees. \n\
-		People donated " + str(sum([n for n in donations.values()])) +\
-		" XLM to " + str(len(donations.keys())) +\
+		"Stats: \n"
+		"Inflation received: " + str(inflation) + "\n"
+		"A total of " + str(total_payments_cost) +
+		" XLM paid over " + str(num_payments) +
+		" inflation payments using " + str(total_fee_cost) + " XLM in fees. \n"
+		"People donated " + str(sum([n for n in donations.values()])) +
+		" XLM to " + str(len(donations.keys())) +
 		" different addresses.\n"
 	)
 
@@ -123,5 +142,8 @@ def main(inflation):
 		json.dump(transactions, outf)
 	print("Done. Output to transactions.json")
 
+
 TEST_AMT = 49855.2650163
-if __name__ == '__main__': main(TEST_AMT)
+
+if __name__ == '__main__':
+	main(TEST_AMT)
